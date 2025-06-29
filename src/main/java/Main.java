b4ed0617a4ff0a53b588f4d0ce833dd6ea528d60
Anna Yakimenko <qyri@ya.ru>
2025-06-29 15:40:59 +0300

    29.06

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,11 @@ import java.sql.*;
 import java.util.Properties;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException, SQLException {
+public static void main(String[] args) throws IOException, SQLException {
 
         //Считать конфиг
         Properties prop = new Properties();
-        InputStream stream = ClassLoader.getSystemResourceAsStream("SQL.properties");
+        InputStream stream = ClassLoader.getSystemResourceAsStream("SQLSettings.properties");
         prop.load(stream);
 
         String url = prop.getProperty("url");
@@ -18,14 +17,31 @@ public class Main {
 
         String sqlQuery = "Select * from animals";
 
-        try {
-            Connection con = DriverManager.getConnection(url, name, password);
-        Statement statement= con.createStatement();
-        ResultSet result= statement.executeQuery(sqlQuery);
-            
+        //Создадим подключение
+        try (Connection con = DriverManager.getConnection(url, name, password)) {
+            Statement statement = con.createStatement();
+
+            //Выполним запрос
+            ResultSet result = statement.executeQuery(sqlQuery);
+
+            System.out.printf("%-3s %-15s %-20s %-15s%n", "id", "color", "name", "type");
+            System.out.println("---------------------------------------------------");
+
+            //Выведем данные на экран
+            while (result.next()) {
+
+                System.out.printf("%-3s %-15s %-20s %-15s%n",
+                        result.getInt("id"),
+                        result.getString("color"),
+                        result.getString("name"),
+                        result.getString("type")
+                );
+            }
+
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
- 
     }
 }
+
+
